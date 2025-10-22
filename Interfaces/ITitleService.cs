345ff1bb2ac0028800cd7f58617c29f056038fca
Emil Vladinov <98382991+emkataumre@users.noreply.github.com>
2025-10-22 15:38:04 +0200

    added pagination

--- Interfaces/ITitleService.cs
+++ Interfaces/ITitleService.cs
@@ -4,12 +4,12 @@ namespace IMDB.DataService.Interfaces;
 
 public interface ITitleService
 {
-    Task<IEnumerable<TitleListDto>> GetAllTitlesAsync(int page = 0, int pageSize = 10);
+    Task<PaginatedResult<TitleListDto>> GetAllTitlesAsync(int page = 0, int pageSize = 10);
     Task<TitleFullDto?> GetTitleByIdAsync(string tconst);
 
-    Task<IEnumerable<TitleListDto>> GetTitlesByGenre(string genreName, int page = 0, int pageSize = 10);
-    Task<IEnumerable<TitleListDto>> GetTitlesByPersonAsync(string nconst, int page = 0, int pageSize = 10);
-    Task<IEnumerable<TitleListDto>> GetTitlesByTypeAsync(string titleType, int page = 0, int pageSize = 10);
+    Task<PaginatedResult<TitleListDto>> GetTitlesByGenre(string genreName, int page = 0, int pageSize = 10);
+    Task<PaginatedResult<TitleListDto>> GetTitlesByPersonAsync(string nconst, int page = 0, int pageSize = 10);
+    Task<PaginatedResult<TitleListDto>> GetTitlesByTypeAsync(string titleType, int page = 0, int pageSize = 10);
 
 
     Task<int> GetTotalTitlesCountAsync();
