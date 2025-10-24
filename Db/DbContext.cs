cd95978847f5cd9ab7e8cc419e587bfabe6b3fc2
emkataumre <emo.vladinov@gmail.com>
2025-10-24 16:30:30 +0200

    removed prod

--- Db/DbContext.cs
+++ Db/DbContext.cs
@@ -1,13 +1,16 @@
 using IMDB.DataService.Db.Entities;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace IMDB.DataService.Db
 {
     public class DatabaseContext : DbContext
     {
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
 
+
         public DatabaseContext() { }
+
 
         public DbSet<Title> Titles => Set<Title>();
         public DbSet<TitleAlias> TitleAliases => Set<TitleAlias>();
@@ -16,15 +19,18 @@ namespace IMDB.DataService.Db
         public DbSet<Profession> Professions => Set<Profession>();
         public DbSet<Country> Countries => Set<Country>();
 
+
         public DbSet<TitlePerson> TitlePeople => Set<TitlePerson>();
         public DbSet<Rating> Ratings => Set<Rating>();
         public DbSet<Episode> Episodes => Set<Episode>();
+
 
         public DbSet<ImdbUser> ImdbUsers => Set<ImdbUser>();
         public DbSet<UserRating> UserRatings => Set<UserRating>();
         public DbSet<BookmarkTitle> BookmarkTitles => Set<BookmarkTitle>();
         public DbSet<BookmarkPerson> BookmarkPeople => Set<BookmarkPerson>();
         public DbSet<SearchHistory> SearchHistories => Set<SearchHistory>();
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
@@ -33,7 +39,9 @@ namespace IMDB.DataService.Db
             var username = Environment.GetEnvironmentVariable("DB_USER");
             var password = Environment.GetEnvironmentVariable("DB_PASS");
 
+
             var connectionString = $"Host={host};Database={database};Username={username};Password={password}";
+
 
             if (!options.IsConfigured)
             {
@@ -41,31 +49,25 @@ namespace IMDB.DataService.Db
             }
         }
 
+
         protected override void OnModelCreating(ModelBuilder mb)
         {
             // table names
-            mb.Entity<Title>().ToTable("title", "prod");
-            mb.Entity<TitleAlias>().ToTable("title_alias", "prod");
-            mb.Entity<Person>().ToTable("person", "prod");
-            mb.Entity<TitlePerson>().ToTable("title_person", "prod");
-            mb.Entity<Genre>().ToTable("genre", "prod");
-            mb.Entity<Profession>().ToTable("profession", "prod");
-            mb.Entity<Country>().ToTable("country", "prod");
-            mb.Entity<Episode>().ToTable("episode", "prod");
-            mb.Entity<Rating>().ToTable("rating", "prod");
-            mb.Entity<ImdbUser>().ToTable("imdb_user", "prod");
-            mb.Entity<UserRating>().ToTable("user_rating", "prod");
-            mb.Entity<BookmarkTitle>().ToTable("bookmark_title", "prod");
-            mb.Entity<BookmarkPerson>().ToTable("bookmark_person", "prod");
-            mb.Entity<SearchHistory>().ToTable("search_history", "prod");
+            mb.Entity<Title>().ToTable("title");
+            mb.Entity<TitleAlias>().ToTable("title_alias");
+            mb.Entity<Person>().ToTable("person");
+            mb.Entity<TitlePerson>().ToTable("title_person");
+            mb.Entity<Genre>().ToTable("genre");
+            mb.Entity<Profession>().ToTable("profession");
+            mb.Entity<Country>().ToTable("country");
+            mb.Entity<Episode>().ToTable("episode");
+            mb.Entity<Rating>().ToTable("rating");
+            mb.Entity<ImdbUser>().ToTable("imdb_user");
+            mb.Entity<UserRating>().ToTable("user_rating");
+            mb.Entity<BookmarkTitle>().ToTable("bookmark_title");
+            mb.Entity<BookmarkPerson>().ToTable("bookmark_person");
+            mb.Entity<SearchHistory>().ToTable("search_history");
 
-            mb.Entity<Title>()
-                .Property(t => t.TitleType)
-                .HasConversion<string>();
-
-            mb.Entity<TitlePerson>()
-                .Property(tp => tp.Category)
-                .HasConversion<string>();
 
             // pks
             mb.Entity<Title>().HasKey(t => t.Tconst);
@@ -83,92 +85,109 @@ namespace IMDB.DataService.Db
             mb.Entity<BookmarkTitle>().HasKey(bt => new { bt.UserId, bt.Tconst });
             mb.Entity<BookmarkPerson>().HasKey(bp => new { bp.UserId, bp.Nconst });
 
+
             // fks and relationships
             mb.Entity<TitleAlias>()
                 .HasOne(t => t.Title)
                 .WithMany()
                 .HasForeignKey(t => t.Tconst);
 
+
             mb.Entity<TitlePerson>()
                 .HasOne(tp => tp.Title)
-                .WithMany(t => t.TitlePeople)
+                .WithMany()
                 .HasForeignKey(tp => tp.Tconst);
+
 
             mb.Entity<TitlePerson>()
                 .HasOne(tp => tp.Person)
                 .WithMany()
                 .HasForeignKey(tp => tp.Nconst);
 
+
             mb.Entity<Episode>()
                 .HasOne(e => e.Title)
                 .WithMany()
                 .HasForeignKey(e => e.Tconst);
+
 
             mb.Entity<Episode>()
                 .HasOne(e => e.ParentTitle)
                 .WithMany()
                 .HasForeignKey(e => e.ParentTconst);
 
+
             mb.Entity<Rating>()
                 .HasOne(r => r.Title)
                 .WithMany()
                 .HasForeignKey(r => r.Tconst);
+
 
             mb.Entity<UserRating>()
                 .HasOne(ur => ur.User)
                 .WithMany()
                 .HasForeignKey(ur => ur.UserId);
 
+
             mb.Entity<UserRating>()
                 .HasOne(ur => ur.Title)
                 .WithMany()
                 .HasForeignKey(ur => ur.Tconst);
+
 
             mb.Entity<BookmarkTitle>()
                 .HasOne(bt => bt.User)
                 .WithMany()
                 .HasForeignKey(bt => bt.UserId);
 
+
             mb.Entity<BookmarkTitle>()
                 .HasOne(bt => bt.Title)
                 .WithMany()
                 .HasForeignKey(bt => bt.Tconst);
+
 
             mb.Entity<BookmarkPerson>()
                 .HasOne(bp => bp.User)
                 .WithMany()
                 .HasForeignKey(bp => bp.UserId);
 
+
             mb.Entity<BookmarkPerson>()
                 .HasOne(bp => bp.Person)
                 .WithMany()
                 .HasForeignKey(bp => bp.Nconst);
+
 
             mb.Entity<SearchHistory>()
                 .HasOne(sh => sh.User)
                 .WithMany()
                 .HasForeignKey(sh => sh.UserId);
 
+
             // many to many relationships
             mb.Entity<Person>()
                 .HasMany(p => p.KnownForTitles)
                 .WithMany(t => t.KnownForByPeople)
-                .UsingEntity(j => j.ToTable("person_known_for_title", "prod"));
+                .UsingEntity(j => j.ToTable("person_known_for_title"));
+
 
             mb.Entity<Person>()
                 .HasMany(p => p.Professions)
                 .WithMany(pr => pr.People)
-                .UsingEntity(j => j.ToTable("person_profession", "prod"));
+                .UsingEntity(j => j.ToTable("person_profession"));
+
 
             mb.Entity<Title>()
                 .HasMany(t => t.Genres)
                 .WithMany(g => g.Titles)
-                .UsingEntity(j => j.ToTable("title_genre", "prod"));
+                .UsingEntity(j => j.ToTable("title_genre"));
+
 
             mb.Entity<Title>()
                 .HasMany(t => t.Countries)
                 .WithMany(c => c.Titles)
-                .UsingEntity(j => j.ToTable("title_country", "prod"));
+                .UsingEntity(j => j.ToTable("title_country"));
         }
     }
 }
