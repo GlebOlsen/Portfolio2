2c956913331f1344595d9a04440da9e647474b4a
emkataumre <emo.vladinov@gmail.com>
2025-10-31 12:12:43 +0100

    imporved new controllers to use the same constructor strucutre as all other controllers and added pagination where it was missing

--- Controllers/EpisodesController.cs
+++ Controllers/EpisodesController.cs
@@ -1,31 +1,34 @@
 using ImdbClone.Api.Interfaces;
+using ImdbClone.Api.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ImdbClone.Api.Controllers;
 
 [ApiController]
 [Route("episodes")]
-public class EpisodesController : ControllerBase
+public class EpisodesController(IEpisodeService episodeService, PaginationService paginationService)
+    : ControllerBase
 {
-    private readonly IEpisodeService _episodeService;
-
-    public EpisodesController(IEpisodeService episodeService)
-    {
-        _episodeService = episodeService;
-    }
-
     [HttpGet]
-    public async Task<IActionResult> GetAllEpisodes([FromQuery] int? page, [FromQuery] int? pageSize)
+    public async Task<IActionResult> GetAllEpisodes(
+        [FromQuery] int? page,
+        [FromQuery] int? pageSize
+    )
     {
-        var result = await _episodeService.GetAllEpisodesAsync(page ?? 0, pageSize ?? 10);
+        var result = await episodeService.GetAllEpisodesAsync(page ?? 0, pageSize ?? 10);
+
+        var queryParams = new Dictionary<string, string?>();
+
+        paginationService.SetPaginationUrls(result, Request.Path, queryParams);
+
         return Ok(result);
     }
 
     [HttpGet("{tconst}")]
     public async Task<IActionResult> GetEpisodeById(string tconst)
     {
-        var result = await _episodeService.GetEpisodeByIdAsync(tconst);
-        
+        var result = await episodeService.GetEpisodeByIdAsync(tconst);
+
         if (result == null)
             return NotFound();
 
